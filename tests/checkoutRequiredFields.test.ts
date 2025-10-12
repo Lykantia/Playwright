e829d3688b2130ab61985298563f3d41b4abdd06
Lykantia <zuza.prokop@seznam.cz>
2025-10-12 20:41:42 +0200

    clean up checkout validation

--- tests/checkoutRequiredFields.test.ts
+++ tests/checkoutRequiredFields.test.ts
@@ -4,10 +4,10 @@ import { LoginPage } from "../pages/LoginPage";
 import { CheckoutPage } from "../pages/CheckoutPage";
 import { checkoutData } from "../data/checkout";
 
-test('test', async ({ page }) => {
+test("Sequential validation of fields in checkout", async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
 
-//login
+  //login
   const loginPage = new LoginPage(page);
 
   await loginPage.goto();
@@ -21,24 +21,26 @@ test('test', async ({ page }) => {
 
   // First Name
   await checkout.continueCheckout();
-  await expect(page.locator('[data-test="error"]'))
-    .toContainText('Error: First Name is required');
+  await expect(page.locator('[data-test="error"]')).toContainText(
+    "Error: First Name is required"
+  );
   await checkout.firstNameInput.fill(checkoutData.valid.firstName);
 
   // Last Name
   await checkout.continueCheckout();
-  await expect(page.locator('[data-test="error"]'))
-    .toContainText('Error: Last Name is required');
+  await expect(page.locator('[data-test="error"]')).toContainText(
+    "Error: Last Name is required"
+  );
   await checkout.lastNameInput.fill(checkoutData.valid.lastName);
 
   // Postal Code
   await checkout.continueCheckout();
-  await expect(page.locator('[data-test="error"]'))
-    .toContainText('Error: Postal Code is required');
+  await expect(page.locator('[data-test="error"]')).toContainText(
+    "Error: Postal Code is required"
+  );
   await checkout.postalCodeInput.fill(checkoutData.valid.lastName);
 
-// Finish
+  // Finish
   await checkout.continueCheckout();
   await expect(checkout.finishButton).toBeVisible();
 });
-
